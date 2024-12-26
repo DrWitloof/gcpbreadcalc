7fc74de1bd05b8346d1a125a918d5de5a3fa1e72
Koen Van Der Auwera <koen.vanderauwera@gmail.com>
2024-12-26 21:12:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -39,5 +39,7 @@ def bereken():
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+    # app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), debug=True)
 
